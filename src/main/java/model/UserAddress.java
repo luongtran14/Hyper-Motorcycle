a3f7152aaf045bb015d19873c8eba87dc0e02a08
tungnt3012 <72959397+tungnt3012@users.noreply.github.com>
2022-10-17 08:19:32 +0700

    Add user address and deactive user for admin
    Add user address and deactive user for admin

---
+++ src/main/java/model/UserAddress.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class UserAddress {
+    private int id;
+    private String fullAddress;
+    private String province;
+    private String city;
+    private String district;
+    private int isMain;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getIsMain() {
+        return isMain;
+    }
+
+    public void setIsMain(int isMain) {
+        this.isMain = isMain;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    
+}
