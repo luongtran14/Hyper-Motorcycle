ca803af088bba2e1fc9d1f7eb2a3c71f01049867
NguyenPhuonl <112892587+NguyenPhuonl@users.noreply.github.com>
2022-11-02 11:31:50 +0700

    update

--- src/main/java/controller/color/DeleteColorController.java
+++ src/main/java/controller/color/DeleteColorController.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller.product;
 
-import dao.CategoryDAO;
-import dao.ProductDAO;
+package controller.color;
+
+import dao.ColorDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -17,52 +16,41 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Category;
-import model.Product;
 
 /**
  *
- * @author Admin
+ * @author nguye
  */
-@WebServlet(name = "ProductAdminDisableController", urlPatterns = {"/admin/products/disable"})
-public class ProductAdminDisableController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+@WebServlet(name="DeleteColorController", urlPatterns={"/deletecolor"})
+public class DeleteColorController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ProductAdminDisableController</title>");            
+            out.println("<title>Servlet DeleteColorController</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ProductAdminDisableController at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet DeleteColorController at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
-    
-    protected void processRequestGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException, ClassNotFoundException, NullPointerException {
-
-      
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -70,23 +58,24 @@ public class ProductAdminDisableController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-         try {
-            int id = Integer.parseInt(request.getParameter("id"));
-            Boolean disable = Boolean.parseBoolean(request.getParameter("disable"));
-            ProductDAO pDao = new ProductDAO();
-            Product product = pDao.getSpecificProductById(id);
-            pDao.changeProductActivationWithProductId(id, disable);
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            response.sendRedirect("../../admin/products?categoryId=0");
+    throws ServletException, IOException {
+        try {
+            String cId =  request.getParameter("colorid");
+            ColorDAO dao = new ColorDAO();
+            dao.deleteColorById(cId);
+            dao.deleteProductColorById(cId);
+            response.sendRedirect("managercolorcontroller");
+        } catch (SQLException ex) {
+            Logger.getLogger(DeleteColorController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DeleteColorController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
+        
+        
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -94,13 +83,12 @@ public class ProductAdminDisableController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
